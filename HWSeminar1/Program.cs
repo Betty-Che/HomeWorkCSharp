046aa0b1fabfea7f20ca1a683019406ae86b2e06
Черепанова Элизабет Юрьевна <betty.che2806@gmail.com>
2023-02-02 19:53:26 +0500

    hw1 all

--- HWSeminar1/Program.cs
+++ HWSeminar1/Program.cs
@@ -1,87 +1,97 @@
-﻿bool isWork = true;
-
-while (isWork)
+﻿internal class Program
 {
-    Console.Write("Введите номер задачи: ");
-
-    if (int.TryParse(Console.ReadLine(), out int i))
+    private static void Main(string[] args)
     {
-        switch (i)
+        bool isWork = true;
+
+        while (isWork)
         {
-            case 1: //Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+            Console.Write("Введите номер задачи: ");
+
+            if (int.TryParse(Console.ReadLine(), out int i))
+            {
+                switch (i)
                 {
-                    Console.WriteLine("Введите значение a: ");
-                    int a = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Введите значение b: ");
-                    int b = Convert.ToInt32(Console.ReadLine());
+                    case 1: //Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+                        {
+                            Console.WriteLine("Введите значение a: ");
+                            int a = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Введите значение b: ");
+                            int b = Convert.ToInt32(Console.ReadLine());
 
-                    int max = 0;
-                    int min = 0;
+                            int max = 0;
+                            int min = 0;
 
-                    if (a > b)
-                    {
-                        max = a;
-                        min = b;
-                    }
-                    else
-                    {
-                        max = b;
-                        min = a;
-                    }
-                    Console.WriteLine("max = " + max + " и " + "min = " + min);
-                    break;
+                            if (a > b)
+                            {
+                                max = a;
+                                min = b;
+                            }
+                            else
+                            {
+                                max = b;
+                                min = a;
+                            }
+                            Console.WriteLine("max = " + max + " и " + "min = " + min);
+                            break;
+                        }
+                    case 2: //Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+                        {
+                            Console.WriteLine("Введите значение a: ");
+                            int a = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Введите значение b: ");
+                            int b = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Введите значение c: ");
+                            int c = Convert.ToInt32(Console.ReadLine());
+
+                            if (a > b && a > c)
+                            {
+                                Console.WriteLine("Максимальное число:" + a);
+                            }
+                            else if (b > a && b > c)
+                            {
+                                Console.WriteLine("Максимальное число:" + b);
+                            }
+                            else if (c > a && c > b)
+                            {
+                                Console.WriteLine("Максимальное число:" + c);
+                            }
+
+                            break;
+                        }
+                    case 3: //Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+                        {
+                            Console.WriteLine("Введите число: ");
+                            int a = Convert.ToInt32(Console.ReadLine());
+                            if (a % 2 == 0)
+                            {
+                                Console.WriteLine("Четное");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Нечетное");
+                            }
+
+                            break;
+                        }
+                    case 4: //Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+                        {
+                            Console.WriteLine("Введите число: ");
+                            int n = int.Parse(Console.ReadLine());
+                            int num = 2;
+
+                            while (num <= n)
+                            if (num % 2 == 0)
+                            {
+                                Console.WriteLine(num + ", ");
+                                num = num + 2;
+                            }
+                            break;
+                        }
+
                 }
-            case 2: //Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-                {
-                    Console.WriteLine("Введите значение a: ");
-                    int a = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Введите значение b: ");
-                    int b = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Введите значение c: ");
-                    int c = Convert.ToInt32(Console.ReadLine());
 
-                    if (a > b && a > c)
-                    {
-                        Console.WriteLine("Максимальное число:" + a);
-                    }
-                    else if (b > a && b > c)
-                    {
-                        Console.WriteLine("Максимальное число:" + b);
-                    }
-                    else if (c > a && c > b)
-                    {
-                        Console.WriteLine("Максимальное число:" + c);
-                    }
-
-                    break;
-                }
-            case 3: //Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-                {
-                    Console.WriteLine("Введите число: ");
-                    int a = Convert.ToInt32(Console.ReadLine());
-                    if (a % 2 == 0)
-                    {
-                        Console.WriteLine("Четное");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Нечетное");
-                    }
-
-                    break;
-                }
-            case 4: //Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-                {
-                    Console.WriteLine("Введите число: ");
-                    int a = Convert.ToInt32(Console.Read());
-                    //int count = 2;
-
-                    //while (count )
-
-                    break;
-                }
-                break;
+            }
         }
-
     }
 }
