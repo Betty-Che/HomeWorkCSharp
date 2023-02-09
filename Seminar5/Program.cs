998cca9d5ee5d3871551ca9d7912e7ea81e11bd3
Черепанова Элизабет Юрьевна <betty.che2806@gmail.com>
2023-02-09 23:57:04 +0500

    sem5+2task

--- Seminar5/Program.cs
+++ Seminar5/Program.cs
@@ -6,10 +6,10 @@
 // 2. Циклом проверяем отрицательные и положительные элементы массива
 // 
 
-int[] array = GetRandomArray(12, -9, 9);
-int[] minAndMax = GetMinAndMaxFromArray(array);
-System.Console.WriteLine($"Введен массив {string.Join("", array)}");
-System.Console.WriteLine($"Сумма положительных {minAndMax[0]}, сумма отрицательных {minAndMax[1]}");
+// int[] array = GetRandomArray(12, -9, 9);
+// int[] minAndMax = GetMinAndMaxFromArray(array);
+// System.Console.WriteLine($"Введен массив {string.Join("", array)}");
+// System.Console.WriteLine($"Сумма положительных {minAndMax[0]}, сумма отрицательных {minAndMax[1]}");
 
 int [] GetRandomArray(int length, int min, int max)
 {
@@ -40,4 +40,48 @@ int [] GetMinAndMaxFromArray(int[] array)
         }
     }
     return minAndMax;
+}
+
+// Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+// int [] array = GetRandomArray(4, -9, 10);
+// int[] changedArray = ChangeMarks(array);
+// System.Console.WriteLine($"Выведены заданные элементы массива {string.Join(" ", array)} и изменненный массив {string.Join(" ", changedArray)}");
+
+int [] ChangeMarks(int[] array)
+{
+    int[] changed = new int [array.Length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        changed[i] = array[i] * (-1);
+    }
+    return changed;
+}
+
+// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+int [] array = GetRandomArray(5, -9, 9);
+int number = -3;
+if(FindNumber(array, number))
+{
+    System.Console.WriteLine($"{string.Join(" ", array)} Да");
+}
+else System.Console.WriteLine($"{string.Join(" ", array)} Нет");
+
+bool FindNumber(int[] array, int number)
+{
+    if (number < 0) number = number * -1;
+    for(int i = 0; i < array.Length; i++)
+    {
+        int modul = array[i];
+        if(modul < 0) modul = modul * -1;
+        if(modul == number)
+        {
+            return true;
+        }
+    }
+    return false;
 }
